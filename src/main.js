f9b6a5465adb750ff2a5c69679396cb188490b68
hanpandeng <yourhan1996@outlook.com>
2018-10-29 16:19:36 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -51,6 +51,7 @@ new Vue({
 })
 
 axios.defaults.baseURL = 'http://192.168.6.137/xtadvert/';
+// axios.defaults.baseURL = 'http://localhost:8081';
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
 axios.interceptors.request.use((config) => {
 	if(config.method === 'post' ){
--- src/store/index.js
+++ src/store/index.js
@@ -6,5 +6,6 @@ Vue.use(vuex);
 export default new vuex.Store({
   state: {
     http: 'http://192.168.6.137/xtadvert/'
+    // http: 'http://localhost:8081'
   }
 })
